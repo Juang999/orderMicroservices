43b39574896b85fc2f29d293d684a41c928c248b
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-05-09 11:32:57 +0700

    rename endpoint

--- routes/partner-address.js
+++ routes/partner-address.js
@@ -4,13 +4,12 @@ const controller = require('../app/controllers/Controller')
 const middleware = require('../app/kernel')
 
 let route = [
-    '/', //0
-    '/:ptnra_oid', //1
-    
+    '/create-address-customer', //0
+    '/detail-address-customer/:ptnra_oid', //1
 ]
 
 router.post(route[0], [middleware.authenticate], controller.PartnerAddressController.create)
 router.get(route[1], [middleware.authenticate], controller.PartnerAddressController.show)
-router.patch(route[1], [middleware.authenticate], controller.PartnerAddressController.activate)
+// router.patch(route[1], [middleware.authenticate], controller.PartnerAddressController.activate)
 
 module.exports = router
--- routes/partner-contact-address.js
+++ routes/partner-contact-address.js
@@ -4,8 +4,8 @@ const middleware = require('../app/kernel')
 const controller = require('../app/controllers/Controller')
 
 let route = [
-    '/', //0 -> toCreateNewContact
-    '/:ptnrac_oid' //1 -> toGetDetailContact
+    '/create-contact-address', //0 -> toCreateNewContact
+    '/detail-contact-address/:ptnrac_oid' //1 -> toGetDetailContact
 ]
 
 router.post(route[0], [middleware.authenticate], controller.ParnterContactController.create)
