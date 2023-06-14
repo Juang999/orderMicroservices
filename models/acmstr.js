76b26ec36dfc6257dbb6010266dc51a2f3a8e054
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-06-14 16:02:44 +0700

    add eslint package

---
+++ models/acmstr.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class AcMstr extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  AcMstr.init({
+    ac_oid: DataTypes.UUID,
+    ac_dom_id: DataTypes.INTEGER,
+    ac_add_by: DataTypes.STRING,
+    ac_add_date: DataTypes.DATE,
+    ac_upd_by: DataTypes.STRING,
+    ac_upd_date: DataTypes.DATE,
+    ac_id: DataTypes.INTEGER,
+    ac_code: DataTypes.STRING,
+    ac_name: DataTypes.INTEGER,
+    ac_desc: DataTypes.INTEGER,
+    ac_parent: DataTypes.INTEGER,
+    ac_type: DataTypes.STRING,
+    ac_is_sumlevel: DataTypes.STRING,
+    ac_sign: DataTypes.STRING,
+    ac_active: DataTypes.STRING,
+    ac_dt: DataTypes.DATE,
+    ac_subclass: DataTypes.INTEGER,
+    ac_subclass_2: DataTypes.INTEGER,
+    ac_subclass_3: DataTypes.INTEGER,
+    ac_cu_id: DataTypes.INTEGER,
+    ac_cash_flow: DataTypes.INTEGER,
+    ac_in_cash_flow: DataTypes.STRING,
+    ac_is_budget: DataTypes.STRING,
+    ac_code_hirarki: DataTypes.STRING,
+    ac_level: DataTypes.INTEGER,
+    ac_priority: DataTypes.INTEGER,
+    ac_is_cf: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'AcMstr',
+  });
+  return AcMstr;
+};
