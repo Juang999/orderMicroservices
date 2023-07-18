38ecc6ac477b4242c219ffd316b29b9836f4263d
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-07-18 09:50:06 +0700

    update controller

--- app/controllers/Controller.js
+++ app/controllers/Controller.js
@@ -2,20 +2,20 @@ const Controller = {
 	Admin: {
 		SalesQuotationController: require('./Admin/SalesQuotationController')
 	},
-	// Client: {
-	// 	ProductController: require('./Client/ProductController'),
-	// 	PriceController: require('./Client/PriceController'),
-	// 	PartnerController: require('./Client/PartnerController'),
-	// 	PlanController: require('./Client/PlanController'),
-	// 	PartnerAddressController: require('./Client/PartnerAddressController'),
-	// 	ParnterContactController: require('./Client/PartnerContactController'),
-	// 	VisitController: require('./Client/VisitController'),
-	// 	SalesQuotationController: require('./Client/SalesQuotationController')
-	// },
-	// Default: {
-	// 	AuthController: require('./AuthController'),
-	// 	MasterController: require('./MasterController'),
-	// }
+	Client: {
+		ProductController: require('./Client/ProductController'),
+		PriceController: require('./Client/PriceController'),
+		PartnerController: require('./Client/PartnerController'),
+		PlanController: require('./Client/PlanController'),
+		PartnerAddressController: require('./Client/PartnerAddressController'),
+		ParnterContactController: require('./Client/PartnerContactController'),
+		VisitController: require('./Client/VisitController'),
+		SalesQuotationController: require('./Client/SalesQuotationController')
+	},
+	Default: {
+		AuthController: require('./AuthController'),
+		MasterController: require('./MasterController'),
+	}
 }
 
 module.exports = Controller
