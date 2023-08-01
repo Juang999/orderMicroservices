41945fe360f5def0e5a2f4f4703f09c26f147f44
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-08-01 11:50:50 +0700

    middleware baru untuk admin

--- app/kernel.js
+++ app/kernel.js
@@ -1,7 +1,8 @@
 const kernel = {
     authenticate: require('./Middleware/authenticate'),
     CheckinRequest: require('./Middleware/requests/CheckinRequest'),
-    CheckoutRequest: require('./Middleware/requests/CheckoutRequest')
+    CheckoutRequest: require('./Middleware/requests/CheckoutRequest'),
+    adminAuthenticate: require('./Middleware/admin-authenticate')
 }
 
 module.exports = kernel
