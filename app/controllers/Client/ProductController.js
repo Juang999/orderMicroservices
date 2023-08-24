e506ba2c5cbdaa3f20c27c914badf72ea427a5c5
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-08-24 14:32:04 +0700

    refactor code

--- app/controllers/Client/ProductController.js
+++ app/controllers/Client/ProductController.js
@@ -1,9 +1,10 @@
 const {PtMstr, EnMstr, CodeMstr, PidDet, PiddDet, InvcMstr, PtCatMstr, PtsCatCat, SizeMstr, Sequelize, PiMstr, LocMstr} = require('../../../models')
 const {Op} = require('sequelize')
 const sequelize = require('sequelize')
+const {page} = require('../../../helper/helper')
 
-const ProductController = {
-	getProductByPriceCategory: async (req, res) => {
+class ProductController {
+	getProductByPriceList = async (req, res) => {
 		try {
 			let page = (req.query.page == null) ? 1 : req.query.page
 
@@ -22,7 +23,6 @@ const ProductController = {
 			if (req.query.entity) where.pt_en_id = {[Op.eq]: req.query.entity}
 			if (req.query.category) where.pt_cat_id = {[Op.eq]: req.query.category}
 			if (req.query.query) where.pt_desc1 = {[Op.like]: `%${req.query.query}%`}
-			if (req.query.subcategory) where.pt_scat_id = {[Op.eq]: req.query.subcategory}
 
 			let {count, rows} = await PtMstr.findAndCountAll({
 				limit: limit,
@@ -80,8 +80,9 @@ const ProductController = {
 					error: error.message
 				})
 		}
-	},
-	showProductByPriceCategory: (req, res) => {
+	}
+
+	showProductByPriceList = (req, res) => {
 		PtMstr.findOne({
 			where: {
 				[Op.and]: {
@@ -101,7 +102,7 @@ const ProductController = {
 					as: 'price',
 					attributes: ['pid_oid', 'pid_pt_id', 'pid_pi_oid'],
 					where: {
-						pid_pi_oid: req.params.pi_oid
+						pid_pi_oid: req.query.price_list
 					},
 					include: [
 						{
@@ -165,13 +166,105 @@ const ProductController = {
 					error: err.message
 				})
 		})
-	},
-	showProductByLocation: (req, res) => {
+	}
+
+	getProductByLocation = (req, res) => {
+		let page = (req.query.page == null) ? 1 : req.query.page
+
+		let limit = 10
+		let offset = (page * limit) - limit
+		let whereLocation = (req.query.loc_id) ? [req.query.loc_id] : [10001, 200010, 300018]
+
+		let where ={
+			pt_id: {
+				[Op.in]: Sequelize.literal(`(SELECT DISTINCT(invc_pt_id) FROM public.invc_mstr WHERE invc_loc_id IN (${whereLocation}))`)
+			}
+		}
+
+		if (req.query.query) {where.pt_desc1 = {[Op.like]: `%${req.query.query}%`}}
+		if (req.query.category) {where.pt_cat_id = {[Op.eq]: req.query.category}}
+		if (req.query.subcategory) {where.pt_scat_id = {[Op.eq]: req.query.subcategory}}
+		if (req.query.entity) {where.pt_en_id = {[Op.eq]: req.query.entity}}
+
+		PtMstr.findAndCountAll({
+			limit: limit,
+			offset: offset,
+			attributes: ['pt_desc2', 'pt_desc1', 'pt_clothes_id', 'pt_en_id', 'pt_id'],
+			order: [['pt_clothes_id', 'asc']],
+			where: where,
+			include: [
+				{
+					model: EnMstr,
+					as: 'EnMstr',
+					attributes: ['en_id', 'en_desc'],
+				}, {
+					model: PtCatMstr,
+					as: 'category_product',
+					attributes: [['ptcat_desc', 'category']]
+				}, {
+					model: PtsCatCat,
+					as: 'sub_category',
+					attributes: [['ptscat_desc', 'sub_category']]
+				}, {
+					model: InvcMstr,
+					as: 'Qty',
+					attributes: ['invc_oid', 'invc_pt_id', 'invc_loc_id'],
+					where: {
+						invc_loc_id: {
+							[Op.in]: whereLocation
+						}
+					},
+					include: [
+						{
+							model: LocMstr,
+							as: 'location',
+							attributes: ['loc_id', 'loc_desc']
+						}
+					]
+				}
+			],
+			distinct: true
+		}).then(result => {
+			let theResult = {
+				data: result.rows,
+				totalData: result.rows.length,
+				page: page,
+				totalPage: Math.ceil(result.count/limit)
+			}
+
+			res.status(200)
+				.json({
+					status: 'berhasil',
+					message: 'berhasil mengambil data produk berdasarkan lokasi',
+					data: theResult
+				})
+		}).catch(err => {
+			res.status(400)
+				.json({
+					status: 'gagal',
+					message: 'gagal mengambil data produk berdasarkan lokasi',
+					error: err.message
+				})
+		})
+	}
+
+	showProductByLocation = (req, res) => {
 		let idLocation
 
-		if (req.params.entity == 1) {idLocation = 10001}
-		if (req.params.entity == 2) {idLocation = 200010}
-		if (req.params.entity == 3) {idLocation = 300018}
+		switch (parseInt(req.query.entity)) {
+			case 1:
+				idLocation = 10001
+				break;
+
+			case 2:
+				idLocation = 200010
+				break
+
+			case 3:
+				idLocation = 300010
+				break
+
+		}
 
 		PtMstr.findOne({
 			where: {
@@ -252,63 +345,9 @@ const ProductController = {
 					error: err.message
 				})
 		})
-	},
-	showSize: (req, res) => {
-		SizeMstr.findAll({
-			where: {
-				size_id: {
-					[Op.in]: sequelize.literal(`(SELECT pt_size_id FROM public.pt_mstr WHERE (pt_desc2 = '${req.params.product}' AND pt_code_color_id = ${req.params.pt_code_color_id}))`)
-				}
-			},
-			attributes: ['size_id', 'size_name'],
-			order: [['size_id', 'asc']]
-		})
-			.then(result => {
-				res.status(200)
-					.json({
-						status: 'success',
-						message: 'berhasil mengambil data',
-						data: result
-					})
-			})
-			.catch(err => {
-				res.status(400)
-					.json({
-						status: 'failed',
-						message: 'gagal mengambil data',
-						error: err.message
-					})
-			})
-	},
-	showGrade: (req, res) => {
-		PtMstr.findAll({
-			where: {
-				[Op.and]: [
-					{pt_desc2: req.params.product},
-					{pt_size_id: req.params.size_id},
-					{pt_code_color_id: req.params.color_id}
-				]
-			},
-			attributes: ['pt_class']
-		})
-			.then(result => {
-				res.status(200)
-					.json({
-						status: 'success',
-						message: 'berhasil mengambil data grade',
-						data: result
-					})
-			})
-			.catch(err => {
-				res.status(400)
-					.json({
-						status: 'failed',
-						message: 'gagal mengambil data grade',
-						error: err.message
-					})
-			})
-	},
-	showPriceAndQty: async (req, res) => {
+	}
+
+	getPriceList = async (req, res) => {
 		PiMstr.findAll({
 			attributes: ['pi_oid', 'pi_desc', 'pi_start_date', 'pi_end_date']
 		}).then(result => {
@@ -326,8 +365,9 @@ const ProductController = {
 					error: err.message
 				})
 		})
-	},
-	getCategory: (req, res) => {
+	}
+
+	getCategory = (req, res) => {
 		PtCatMstr.findAll({
 			attributes: ['ptcat_id', 'ptcat_desc']
 		}).then(result => {
@@ -345,8 +385,9 @@ const ProductController = {
 					error: err.message
 				})
 		})
-	},
-	getSubCategory: (req, res) => {
+	}
+
+	getSubCategory = (req, res) => {
 		PtsCatCat.findAll({
 			attributes: ['ptscat_id', 'ptscat_desc'],
 			where: {
@@ -367,10 +408,12 @@ const ProductController = {
 					error: err.message
 				})
 		})
-	},
-	getSize: (req, res) => {
+	}
+
+	getSize = (req, res) => {
 		SizeMstr.findAll({
-			attributes: ['size_id', 'size_code', 'size_name', 'size_desc']
+			attributes: ['size_id', 'size_code', 'size_name', 'size_desc'],
+			order: [['size_name', 'asc']]
 		}).then(result => {
 			res.status(200)
 				.json({
@@ -386,59 +429,18 @@ const ProductController = {
 					error: err.message
 				})
 		})
-	},
-	getProductWithCategory: (req, res) => {
-		let page = (req.query.page) ? req.query.page : 1
-		let offset = page * 10 - 10
-		let limit = 10
-        
-		PtMstr.findAll({
-			where: {
-				pt_group: req.params.category_id
-			},
-			attributes: ['pt_id', 'pt_desc1', 'pt_desc2', 'pt_clothes_id'],
-			include: [
-				{
-					model: CodeMstr,
-					as: 'category',
-					attributes: ['code_name']
-				}
-			],
-			limit: limit,
-			offset: offset
-		}).then(result => {
-			let data = {
-				theData: result,
+	}
 
-				page: page,
-				data: 10
-			}
-
-			res.status(200)
-				.json({
-					status: 'success',
-					message: 'berhasil mengambil data',
-					data: data
-				})
-		}).catch(err => {
-			res.status(400)
-				.json({
-					status: 'failed',
-					messgae: 'gagal mengambil data',
-					error: err.message
-				})
-		})
-	},
-	getAllProduct: (req, res) => {
-		let page = (req.query.page) ? req.query.page : 1
-		let limit = 20
-		let offset = (limit * page) - limit
+	getAllProduct = (req, res) => {
+		let current_page = (req.query.page) ? req.query.page : 1
+		let {page, limit, offset} = page(current_page, limit)
 
 		let where = {pt_pl_id: 1, pt_desc1: {[Op.not]: null}}
-    
+
 		if (req.query.entity > 0) where.pt_en_id = req.query.entity
-		if (req.query.query) where.pt_desc2 = {[Op.like]: `%${req.query.query}%`}
 		if (req.query.category) where.pt_cat_id = req.query.category
+		if (req.query.subcategory) where.pt_scat_id = req.query.subcategory
+		if (req.query.query) where.pt_desc2 = {[Op.like]: `%${req.query.query}%`}
 
 		PtMstr.findAndCountAll({
 			limit: limit,
@@ -520,87 +522,36 @@ const ProductController = {
 					error: err.message
 				})
 		})
-	},
-	getProductByLocation: (req, res) => {
-		let page = (req.query.page == null) ? 1 : req.query.page
+	}
 
-		let limit = 10
-		let offset = (page * limit) - limit
-		let whereLocation = (req.query.loc_id) ? [req.query.loc_id] : [10001, 200010, 300018]
-
-		let where ={
-			pt_id: {
-				[Op.in]: Sequelize.literal(`(SELECT DISTINCT(invc_pt_id) FROM public.invc_mstr WHERE invc_loc_id IN (${whereLocation}))`)
-			}
-		}
-
-		if (req.query.query) {where.pt_desc1 = {[Op.like]: `%${req.query.query}%`}}
-		if (req.query.category) {where.pt_cat_id = {[Op.eq]: req.query.category}}
-		if (req.query.subcategory) {where.pt_scat_id = {[Op.eq]: req.query.subcategory}}
-		if (req.query.entity) {where.pt_en_id = {[Op.eq]: req.query.entity}}
-
-		PtMstr.findAndCountAll({
-			limit: limit,
-			offset: offset,
-			attributes: ['pt_desc2', 'pt_desc1', 'pt_clothes_id', 'pt_en_id', 'pt_id'],
-			order: [['pt_clothes_id', 'asc']],
-			where: where,
-			include: [
-				{
-					model: EnMstr,
-					as: 'EnMstr',
-					attributes: ['en_id', 'en_desc'],
-				}, {
-					model: PtCatMstr,
-					as: 'category_product',
-					attributes: [['ptcat_desc', 'category']]
-				}, {
-					model: PtsCatCat,
-					as: 'sub_category',
-					attributes: [['ptscat_desc', 'sub_category']]
-				}, {
-					model: InvcMstr,
-					as: 'Qty',
-					attributes: ['invc_oid', 'invc_pt_id', 'invc_loc_id'],
-					where: {
-						invc_loc_id: {
-							[Op.in]: whereLocation
-						}
-					},
-					include: [
-						{
-							model: LocMstr,
-							as: 'location',
-							attributes: ['loc_id', 'loc_desc']
-						}
-					]
-				}
+	getGrade = (req, res) => {
+		PtMstr.findAll({
+			attributes: [
+				[Sequelize.literal(`distinct pt_class`), 'grade']
 			],
-			distinct: true
-		}).then(result => {
-			let theResult = {
-				data: result.rows,
-				totalData: result.rows.length,
-				page: page,
-				totalPage: Math.ceil(result.count/limit)
+			where: {
+				pt_class: {
+					[Op.not]: null
+				}
 			}
-
+		})
+		.then(result => {
 			res.status(200)
 				.json({
-					status: 'berhasil',
-					message: 'berhasil mengambil data produk berdasarkan lokasi',
-					data: theResult
+					status: 'sucess!',
+					data: result,
+					error: null
 				})
-		}).catch(err => {
+		})
+		.catch(err => {
 			res.status(400)
-				.json({
-					status: 'gagal',
-					message: 'gagal mengambil data produk berdasarkan lokasi',
+				.josn({
+					status: 'failed!',
+					data: null,
 					error: err.message
 				})
 		})
-
 	}
 }
 
-module.exports = ProductController
+module.exports = new ProductController()
--- routes/Client/product-knowledge.js
+++ routes/Client/product-knowledge.js
@@ -2,33 +2,24 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../../app/controllers/Controller')
 const middleware = require('../../app/kernel')
+const {Client} = require('../route')
 
-const route = [
-	'/get-product-by-price-category', //0
-	'/show-product-by-price-category/:pt_id/pi_oid/:pi_oid', //1
-	'/product/:product/color/:pt_code_color_id', //2
-	'/get-price-list', //3
-	'/product/:product/color/:color_id/size/:size_id', //4
-	'/category', //5
-	'/product/category/:category_id', //6
-	'/category/sub_category/:cat_id', //7
-	'/size', //8
-	'/get-all-product', //9
-	'/get-product-by-location', //10
-	'/show-product-by-location/:pt_id/entity/:entity', //11
-]
+/*
+	API to get product for catalog
+*/
 
-router.get(route[0], [middleware.authenticate], controller.Client.ProductController.getProductByPriceCategory)
-router.get(route[1], [middleware.authenticate], controller.Client.ProductController.showProductByPriceCategory)
-router.get(route[2], [middleware.authenticate], controller.Client.ProductController.showSize)
-// router.get(route[3], [middleware.authenticate], controller.ProductController.showPrice)
-router.get(route[4], [middleware.authenticate], controller.Client.ProductController.showGrade)
-router.get(route[5], controller.Client.ProductController.getCategory)
-router.get(route[6], [middleware.authenticate], controller.Client.ProductController.getProductWithCategory)
-router.get(route[7], [middleware.authenticate], controller.Client.ProductController.getSubCategory)
-router.get(route[8], [middleware.authenticate], controller.Client.ProductController.getSize)
-router.get(route[9], controller.Client.ProductController.getAllProduct)
-router.get(route[10], [middleware.authenticate], controller.Client.ProductController.getProductByLocation)
-router.get(route[11], [middleware.authenticate], controller.Client.ProductController.showProductByLocation)
+// don't need token to access API
+router.get(Client.feature.product.product_size, controller.Client.ProductController.getSize)
+router.get(Client.feature.product.product_grade, controller.Client.ProductController.getGrade)
+router.get(Client.feature.product.product_index, controller.Client.ProductController.getAllProduct)
+router.get(Client.feature.product.product_category, controller.Client.ProductController.getCategory)
+router.get(Client.feature.product.product_sub_category, controller.Client.ProductController.getSubCategory)
+
+// need token to access API
+router.get(Client.feature.product.product_price_list, [middleware.authenticate], controller.Client.ProductController.getPriceList)
+router.get(Client.feature.product.product_by_location, [middleware.authenticate], controller.Client.ProductController.getProductByLocation)
+router.get(Client.feature.product.product_by_price_list, [middleware.authenticate], controller.Client.ProductController.getProductByPriceList)
+router.get(Client.feature.product.product_detail_by_location, [middleware.authenticate], controller.Client.ProductController.showProductByLocation)
+router.get(Client.feature.product.product_detail_by_price_list, [middleware.authenticate], controller.Client.ProductController.showProductByPriceList)
 
 module.exports = router
