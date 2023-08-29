a28c5d0efb686d44cb1f2e80b7a4d5a1bfbaedea
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-08-29 13:23:17 +0700

    refactor kernel file

--- app/kernel.js
+++ app/kernel.js
@@ -1,8 +1,12 @@
-const kernel = {
-    authenticate: require('./Middleware/authenticate'),
-    CheckinRequest: require('./Middleware/requests/CheckinRequest'),
-    CheckoutRequest: require('./Middleware/requests/CheckoutRequest'),
-    adminAuthenticate: require('./Middleware/admin-authenticate')
+class Kernel {
+    constructor () {
+        return {
+            authenticate: require('./Middleware/authenticate'),
+            CheckinRequest: require('./Middleware/requests/CheckinRequest'),
+            CheckoutRequest: require('./Middleware/requests/CheckoutRequest'),
+            adminAuthenticate: require('./Middleware/admin-authenticate')
+        }
+    }
 }
 
-module.exports = kernel
+module.exports = new Kernel()
