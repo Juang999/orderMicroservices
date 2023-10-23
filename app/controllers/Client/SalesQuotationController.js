1b92613536b13237be5682a87803107eb25534e4
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-10-23 13:47:17 +0700

    scale up controller

--- app/controllers/Client/SalesQuotationController.js
+++ app/controllers/Client/SalesQuotationController.js
@@ -350,19 +350,22 @@ class SalesQuotationController {
 		try {
 			// userAccount and get customer data
 			let authUser = await helper.auth(req.get('authorization'))
-			let partnerCustomer = await PtnrMstr.findOne({
-				where: {
-					ptnr_id: req.body.sq_ptnr_id_sold
-				}
+
+			// start checkLimitDebtCustomer
+			let checkDebt = await this.checkDebtCustomer({
+				ptnrId: req.body.sq_ptnr_id_sold, 	
+				sqTotal: req.body.sq_total,
 			})
-	
-			// checkLimitDebtCustomer
-			await this.checkDebtCustomer({
-				ptnrId: partnerCustomer.ptnr_id,
-				ptnrPtnrgId: partnerCustomer.ptnr_ptnrg_id,
-				ptnrLimitCredit: partnerCustomer.ptnr_limit_credit,
-				sqTotal: req.body.sq_total
-			})
+
+			if (checkDebt['status'] == true) {
+				res.status(400)
+					.json({
+						status: 'total pembelian melebihi credit limuit',
+						data: null,
+						error: null
+					})
+			}
+			// end check debt customer
 	
 			// startTransaction, count data SQ, and create sqCode
 			transaction = await sequelize.transaction()
@@ -370,110 +373,110 @@ class SalesQuotationController {
 			const sqCode = `SQ${authUser.detail_user.ptnr_en_id}0${moment().format('MMYY')}00${countDataSQ + 1}`
 
 			let headerSalesQuotation = await SqMstr.create({
-			sq_oid: uuidv4(),
-			sq_dom_id: authUser.detail_user.ptnr_dom_id,
-			sq_en_id: authUser.detail_user.ptnr_en_id,
-			sq_add_by: authUser.usernama,
-			sq_add_date: moment().format('YYYY-MM-DD HH:mm:ss'),
-			sq_code: sqCode,
-			sq_ptnr_id_sold: parseInt(req.body.sq_ptnr_id_sold),
-			sq_ptnr_id_bill: parseInt(req.body.sq_ptnr_id_sold),
-			sq_date: moment().format('YYYY-MM-DD'),
-			sq_si_id: 992,
-			sq_type: 'R',
-			sq_sales_person: authUser.user_ptnr_id,
-			sq_pi_id: req.body.sq_pi_id,
-			sq_pay_type: 9942,
-			sq_pay_method: parseInt(req.body.sq_pay_method),
-			sq_dp: 0,
-			sq_disc_header: 0,
-			sq_total: parseInt(req.body.sq_total),
-			sq_due_date: moment().format('YYYY-MM-DD'),
-			sq_close_date: moment().add(3, 'days').format('YYYY-MM-DD'),
-			sq_trans_id: 'D',
-			sq_trans_rmks: (req.body.sq_trans_rmks) ? req.body.sq_trans_rmks : null,
-			sq_dt: moment().format('YYYY-MM-DD HH:mm:ss'),
-			sq_cu_id: 1,
-			sq_total_ppn: 0,
-			sq_total_pph: 0,
-			sq_payment: 0,
-			sq_exc_rate: parseInt(req.body.sq_cu_id),  
-			sq_tax_inc: null,
-			sq_cons: req.body.sq_cons,
-			sq_terbilang: req.body.sq_terbilang,
-			sq_credit_term: req.body.sq_credit_term,
-			sq_interval: 0,
-			sq_ar_ac_id: 13,
-			sq_ar_sb_id: 0,
-			sq_ar_cc_id: 0,
-			sq_need_date: moment(req.body.sq_need_date).format('YYYY-MM-DD'),
-			sq_is_package: 'N',
-			sq_sales_program: '-',
-			sq_booking: req.body.sq_booking,
-			sq_book_start_date: (req.body.due_date) ? req.body.due_date : moment().format('YYYY-MM-DD HH:mm:ss'),
-			sq_book_end_date: moment(req.body.sq_close_date).format('YYYY-MM-DD'),
-			sq_alocated: req.body.sq_alocated,
-			sq_ptsfr_loc_id: parseInt(req.body.sq_loc_id),
-			sq_ptsfr_loc_to_id: 10001,
-			sq_ptsfr_loc_git: 10004,
-			sq_en_to_id: partnerCustomer.ptnr_en_id,
-			sq_dropshipper: req.body.sq_is_dropshipper,
-			sq_ship_to: (req.body.sq_is_dropshipper == 'Y' ) ? req.body.sq_ship_to : null
-		}, {
-			logging: async (sql, queryObject) => {
-				// value ada 45
-				let value = queryObject.bind
-				await helper.Query.insert(sql, {
-					bind: {
-						$1: value[0],
-						$2: value[1],
-						$3: value[2],
-						$4: value[3],
-						$5: value[4],
-						$6: value[5],
-						$7: value[6],
-						$8: value[7],
-						$9: value[8],
-						$10: value[9],
-						$11: value[10],
-						$12: value[11],
-						$13: value[12],
-						$14: value[13],
-						$15: value[14],
-						$16: value[15],
-						$17: value[16],
-						$18: value[17],
-						$19: value[18],
-						$20: value[19],
-						$21: value[20],
-						$22: value[21],
-						$23: value[22],
-						$24: value[23],
-						$25: value[24],
-						$26: value[25],
-						$27: value[26],
-						$28: value[27],
-						$29: value[28],
-						$30: value[29],
-						$31: value[30],
-						$32: value[31],
-						$33: value[32],
-						$34: value[33],
-						$35: value[34],
-						$36: value[35],
-						$37: value[36],
-						$38: value[37],
-						$39: value[38],
-						$40: value[39],
-						$41: value[40],
-						$42: value[41],
-						$43: value[42],
-						$44: value[43],
-						$45: value[44],
-					}
-				})
-			}
-		})
+				sq_oid: uuidv4(),
+				sq_dom_id: authUser.detail_user.ptnr_dom_id,
+				sq_en_id: authUser.detail_user.ptnr_en_id,
+				sq_add_by: authUser.usernama,
+				sq_add_date: moment().format('YYYY-MM-DD HH:mm:ss'),
+				sq_code: sqCode,
+				sq_ptnr_id_sold: parseInt(req.body.sq_ptnr_id_sold),
+				sq_ptnr_id_bill: parseInt(req.body.sq_ptnr_id_sold),
+				sq_date: moment().format('YYYY-MM-DD'),
+				sq_si_id: 992,
+				sq_type: 'R',
+				sq_sales_person: authUser.user_ptnr_id,
+				sq_pi_id: req.body.sq_pi_id,
+				sq_pay_type: 9942,
+				sq_pay_method: parseInt(req.body.sq_pay_method),
+				sq_dp: 0,
+				sq_disc_header: 0,
+				sq_total: parseInt(req.body.sq_total),
+				sq_due_date: moment().format('YYYY-MM-DD'),
+				sq_close_date: moment().add(3, 'days').format('YYYY-MM-DD'),
+				sq_trans_id: 'D',
+				sq_trans_rmks: (req.body.sq_trans_rmks) ? req.body.sq_trans_rmks : null,
+				sq_dt: moment().format('YYYY-MM-DD HH:mm:ss'),
+				sq_cu_id: 1,
+				sq_total_ppn: 0,
+				sq_total_pph: 0,
+				sq_payment: 0,
+				sq_exc_rate: parseInt(req.body.sq_cu_id),  
+				sq_tax_inc: null,
+				sq_cons: req.body.sq_cons,
+				sq_terbilang: req.body.sq_terbilang,
+				sq_credit_term: req.body.sq_credit_term,
+				sq_interval: 0,
+				sq_ar_ac_id: 13,
+				sq_ar_sb_id: 0,
+				sq_ar_cc_id: 0,
+				sq_need_date: moment(req.body.sq_need_date).format('YYYY-MM-DD'),
+				sq_is_package: 'N',
+				sq_sales_program: '-',
+				sq_booking: req.body.sq_booking,
+				sq_book_start_date: (req.body.due_date) ? req.body.due_date : moment().format('YYYY-MM-DD HH:mm:ss'),
+				sq_book_end_date: moment(req.body.sq_close_date).format('YYYY-MM-DD'),
+				sq_alocated: req.body.sq_alocated,
+				sq_ptsfr_loc_id: parseInt(req.body.sq_loc_id),
+				sq_ptsfr_loc_to_id: 10001,
+				sq_ptsfr_loc_git: 10004,
+				sq_en_to_id: checkDebt['ptnr_en_id'],
+				sq_dropshipper: req.body.sq_is_dropshipper,
+				sq_ship_to: (req.body.sq_is_dropshipper == 'Y' ) ? req.body.sq_ship_to : null
+			}, {
+				logging: async (sql, queryObject) => {
+					// value ada 45
+					let value = queryObject.bind
+					await helper.Query.insert(sql, {
+						bind: {
+							$1: value[0],
+							$2: value[1],
+							$3: value[2],
+							$4: value[3],
+							$5: value[4],
+							$6: value[5],
+							$7: value[6],
+							$8: value[7],
+							$9: value[8],
+							$10: value[9],
+							$11: value[10],
+							$12: value[11],
+							$13: value[12],
+							$14: value[13],
+							$15: value[14],
+							$16: value[15],
+							$17: value[16],
+							$18: value[17],
+							$19: value[18],
+							$20: value[19],
+							$21: value[20],
+							$22: value[21],
+							$23: value[22],
+							$24: value[23],
+							$25: value[24],
+							$26: value[25],
+							$27: value[26],
+							$28: value[27],
+							$29: value[28],
+							$30: value[29],
+							$31: value[30],
+							$32: value[31],
+							$33: value[32],
+							$34: value[33],
+							$35: value[34],
+							$36: value[35],
+							$37: value[36],
+							$38: value[37],
+							$39: value[38],
+							$40: value[39],
+							$41: value[40],
+							$42: value[41],
+							$43: value[42],
+							$44: value[43],
+							$45: value[44],
+						}
+					})
+				}
+			})
 	
 			// inputProductToTable: public.sqd_det	
 			await this.inputProductToDetailSalesQuotation({
@@ -511,6 +514,20 @@ class SalesQuotationController {
 	}
 
 	checkDebtCustomer = async (dataCheck) => {
+		let partnerCustomer = await PtnrMstr.findOne({
+			attributes: [
+					'ptnr_en_id',
+					[Sequelize.literal('CASE WHEN ptnr_limit_credit = 0 THEN ptnr_group.ptnrg_limit_credit END'), 'limit_credit']
+				],
+			include: [
+					{
+						model: PtnrgGrp,
+						as: 'ptnr_group',
+						attributes: []	
+					}
+				]
+		})
+
 		let debt = await ArMstr.findAll({
 			where: {
 				[Op.and]: {
@@ -522,231 +539,64 @@ class SalesQuotationController {
 			},
 			attributes: [[Sequelize.fn('sum', Sequelize.literal('ar_amount - ar_pay_amount')), 'debt_total']]
 		})
-	
-		let partnerGroup = await PtnrgGrp.findOne({
-			where: {
-				ptnrg_id: dataCheck.ptnrPtnrgId
-			}
-		})
-	
-		let total = debt.debt_total + dataCheck.sqTotal
-		var limit
-	
-		if (dataCheck.ptnrLimitCredit > 0) {
-			if ( total >= dataCheck.ptnrLimitCredit) {
-				limit = {
-					status: 'gagal',
-					error: 'total dari pembelian barang dan piutangmu melebihi limit'
-				}
-				return limit
-				return
-			}
-		}
-	
-		if (partnerGroup.ptnrg_limit_credit  > 0) {
-			if (total >= partnerGroup.ptnrg_limit_credit) {
-				limit = {
-					status: 'gagal',
-					error: 'total dari pembelian barang dan piutangmu melebihi limit'
-				}
-	
-				return limit
-				return
-			}
-		}
-	}
 
-	createHeaderSalesQuotation = async (dataHeader) => {
-		let headerSalesQuotation = await SqMstr.create({
-			sq_oid: uuidv4(),
-			sq_dom_id: dataHeader.authUser.detail_user.ptnr_dom_id,
-			sq_en_id: dataHeader.authUser.detail_user.ptnr_en_id,
-			sq_add_by: dataHeader.authUser.usernama,
-			sq_add_date: moment().format('YYYY-MM-DD HH:mm:ss'),
-			sq_code: dataHeader.sqCode,
-			sq_ptnr_id_sold: parseInt(dataHeader.bodyHeader.sq_ptnr_id_sold),
-			sq_ptnr_id_bill: parseInt(dataHeader.bodyHeader.sq_ptnr_id_sold),
-			sq_date: moment().format('YYYY-MM-DD'),
-			sq_si_id: 992,
-			sq_type: 'R',
-			sq_sales_person: dataHeader.authUser.user_ptnr_id,
-			sq_pi_id: dataHeader.bodyHeader.sq_pi_id,
-			sq_pay_type: 9942,
-			sq_pay_method: parseInt(dataHeader.bodyHeader.sq_pay_method),
-			sq_dp: 0,
-			sq_disc_header: 0,
-			sq_total: parseInt(dataHeader.bodyHeader.sq_total),
-			sq_due_date: moment().format('YYYY-MM-DD'),
-			sq_close_date: moment().add(3, 'days').format('YYYY-MM-DD'),
-			sq_trans_id: 'D',
-			sq_trans_rmks: (dataHeader.bodyHeader.sq_trans_rmks) ? dataHeader.bodyHeader.sq_trans_rmks : null,
-			sq_dt: moment().format('YYYY-MM-DD HH:mm:ss'),
-			sq_cu_id: 1,
-			sq_total_ppn: 0,
-			sq_total_pph: 0,
-			sq_payment: 0,
-			sq_exc_rate: parseInt(dataHeader.bodyHeader.sq_cu_id),  
-			sq_tax_inc: null,
-			sq_cons: dataHeader.bodyHeader.sq_cons,
-			sq_terbilang: dataHeader.bodyHeader.sq_terbilang,
-			sq_credit_term: dataHeader.bodyHeader.sq_credit_term,
-			sq_interval: 0,
-			sq_ar_ac_id: 13,
-			sq_ar_sb_id: 0,
-			sq_ar_cc_id: 0,
-			sq_need_date: moment(dataHeader.bodyHeader.sq_need_date).format('YYYY-MM-DD'),
-			sq_is_package: 'N',
-			sq_sales_program: '-',
-			sq_booking: dataHeader.bodyHeader.sq_booking,
-			sq_book_start_date: (dataHeader.bodyHeader.due_date) ? dataHeader.bodyHeader.due_date : moment().format('YYYY-MM-DD HH:mm:ss'),
-			sq_book_end_date: moment(dataHeader.bodyHeader.sq_close_date).format('YYYY-MM-DD'),
-			sq_alocated: dataHeader.bodyHeader.sq_alocated,
-			sq_ptsfr_loc_id: parseInt(dataHeader.bodyHeader.sq_loc_id),
-			sq_ptsfr_loc_to_id: 10001,
-			sq_ptsfr_loc_git: 10004,
-			sq_en_to_id: dataHeader.partnerCustomer.ptnr_en_id,
-			sq_dropshipper: dataHeader.bodyHeader.sq_is_dropshipper,
-			sq_ship_to: (dataHeader.bodyHeader.sq_is_dropshipper == 'Y' ) ? dataHeader.bodyHeader.sq_ship_to : null
-		}, {
-			logging: async (sql, queryObject) => {
-				// value ada 45
-				let value = queryObject.bind
-				await helper.Query.insert(sql, {
-					bind: {
-						$1: value[0],
-						$2: value[1],
-						$3: value[2],
-						$4: value[3],
-						$5: value[4],
-						$6: value[5],
-						$7: value[6],
-						$8: value[7],
-						$9: value[8],
-						$10: value[9],
-						$11: value[10],
-						$12: value[11],
-						$13: value[12],
-						$14: value[13],
-						$15: value[14],
-						$16: value[15],
-						$17: value[16],
-						$18: value[17],
-						$19: value[18],
-						$20: value[19],
-						$21: value[20],
-						$22: value[21],
-						$23: value[22],
-						$24: value[23],
-						$25: value[24],
-						$26: value[25],
-						$27: value[26],
-						$28: value[27],
-						$29: value[28],
-						$30: value[29],
-						$31: value[30],
-						$32: value[31],
-						$33: value[32],
-						$34: value[33],
-						$35: value[34],
-						$36: value[35],
-						$37: value[36],
-						$38: value[37],
-						$39: value[38],
-						$40: value[39],
-						$41: value[40],
-						$42: value[41],
-						$43: value[42],
-						$44: value[43],
-						$45: value[44],
-					}
-				})
-			}
-		})
+		let totalLimit = partnerCustomer['dataValues']['limit_credit']
+		let totalDebt = debt[0]['dataValues']['debt_total']
 	
-		return headerSalesQuotation
+		return (totalLimit != 0 && dataCheck.sqTotal + totalDebt >= totalLimit) ? {status: true, ptnr_en_id: partnerCustomer['dataValues']['ptnr_en_id']} 
+																				: {status: false, ptnr_en_id: partnerCustomer['dataValues']['ptnr_en_id']}
 	}
 
 	inputProductToDetailSalesQuotation = async (data) => {
 		let convertToJson = JSON.parse(data.bodySQ)
+		let sequence = 1
 
 		for (const bodySalesQuotation of convertToJson) {
-			let countDetailProductSalesQuotation = await this.countDetailSalesQuotation(data.sqOid)
 			let costProductSalesQuotation = await this.getInvctTable(bodySalesQuotation.sqd_pt_id)
-			let sequenceDetailProductSalesQuotation = (countDetailProductSalesQuotation) ? countDetailProductSalesQuotation + 1 : 1
 			let unitMeasureProduct = await this.getUnitMeasureProduct(bodySalesQuotation.sqd_pt_id)
-	
-			let dataUpdateQtyProduct = {
-				sqdPtId: bodySalesQuotation.sqd_pt_id,
-				sqdLocId: bodySalesQuotation.sqd_loc_id,
-				sqdQtyBooking: bodySalesQuotation.sqd_qty_booking
+
+			if (data.isBooking == 'Y') {
+				await this.updateStockProduct({
+					sqdPtId: bodySalesQuotation.sqd_pt_id,
+					sqdLocId: bodySalesQuotation.sqd_loc_id,
+					sqdQtyBooking: bodySalesQuotation.sqd_qty_booking
+				})
 			}
-			if (data.isBooking == 'Y') {await this.updateStockProduct(dataUpdateQtyProduct)}
-	
-			await SqdDet.create({
-				sqd_oid: uuidv4(),
-				sqd_dom_id: data.sqDomId,
-				sqd_en_id: data.sqEnId,
-				sqd_add_by: data.userName,
-				sqd_add_date: moment().format('YYYY-MM-DD HH:mm:ss'),
-				sqd_sq_oid: data.sqOid,
-				sqd_seq: parseInt(sequenceDetailProductSalesQuotation),
-				sqd_is_additional_charge: 'N',
-				sqd_si_id: 992,
-				sqd_pt_id: bodySalesQuotation.sqd_pt_id,
-				sqd_rmks: '',
-				sqd_qty: bodySalesQuotation.sqd_qty,
-				sqd_qty_booking: (data.isBooking) ? bodySalesQuotation.sqd_qty : 0,
-				sqd_loc_id: bodySalesQuotation.sqd_loc_id,
-				sqd_cost: costProductSalesQuotation.invct_cost,
-				sqd_price: bodySalesQuotation.sqd_price,
-				sqd_sales_ac_id: 13,
-				sqd_sales_sb_id: 0,
-				sqd_sales_cc_id: 0,
-				sqd_um: unitMeasureProduct.pt_um,
-				sqd_um_conv: 1,
-				sqd_taxable: 'N',
-				sqd_tax_inc: 'N',
-				sqd_tax_class: 9949,
-				sqd_dt: moment().format('YYYY-MM-DD HH:mm:ss'),
-				sqd_payment: 0,
-				sqd_dp: 0,
-				sqd_sales_unit: 0,
-			}, {
-				logging: async (sql, queryObject) => {
-					let value = queryObject.bind
-					await helper.Query.insert(sql, {
-						bind: {
-							$1: value[0],
-							$2: value[1],
-							$3: value[2],
-							$4: value[3],
-							$5: value[4],
-							$6: value[5],
-							$7: value[6],
-							$8: value[7],
-							$9: value[8],
-							$10: value[9],
-							$11: value[10],
-							$12: value[11],
-							$13: value[12],
-							$14: value[13],
-							$15: value[14],
-							$16: value[15],
-							$17: value[16],
-							$18: value[17],
-							$19: value[18],
-							$20: value[19],
-							$21: value[20],
-							$22: value[21],
-							$23: value[22],
-							$24: value[23],
-							$25: value[24],
-							$26: value[25]
-						}
-					})
-				}
-			})
+
+			bodySalesQuotation['sqd_oid'] = uuidv4()
+			bodySalesQuotation['sqd_dom_id'] = data['sqDomId']
+			bodySalesQuotation['sqd_en_id'] = data['sqEnId']
+			bodySalesQuotation['sqd_add_by'] = data['userName']
+			bodySalesQuotation['sqd_add_date'] = moment().format('YYYY-MM-DD HH:mm:ss')
+			bodySalesQuotation['sqd_sq_oid'] = data['sqOid']
+			bodySalesQuotation['sqd_seq'] = parseInt(sequence)
+			bodySalesQuotation['sqd_is_additional_charge'] = 'N'
+			bodySalesQuotation['sqd_si_id'] = 992
+			bodySalesQuotation['sqd_pt_id'] = bodySalesQuotation['sqd_pt_id']
+			bodySalesQuotation['sqd_rmks'] = ''
+			bodySalesQuotation['sqd_qty_booking'] = (data['isBooking']) ? bodySalesQuotation.sqd_qty : 0
+			bodySalesQuotation['sqd_sales_ac_id'] = 13
+			bodySalesQuotation['sqd_sales_sb_id'] = 0
+			bodySalesQuotation['sqd_sales_cc_id'] = 0
+			bodySalesQuotation['sqd_um_conv'] = 1
+			bodySalesQuotation['sqd_taxable'] = 'N'
+			bodySalesQuotation['sqd_tax_inc'] = 'N'
+			bodySalesQuotation['sqd_tax_class'] = 9949
+			bodySalesQuotation['sqd_dt'] = moment().format('YYYY-MM-DD HH:mm:ss')
+			bodySalesQuotation['sqd_payment'] = 0
+			bodySalesQuotation['sqd_dp'] = 0
+			bodySalesQuotation['sqd_sales_unit'] = 0
+			bodySalesQuotation['sqd_cost'] = costProductSalesQuotation['invct_cost']
+			bodySalesQuotation['sqd_um'] = unitMeasureProduct['pt_um']
+
+			sequence += 1
 		}
+
+		await SqdDet.bulkCreate(convertToJson, {
+			logging: async (sql) => {
+				await helper.Query.queryBulkCreate(sql)
+			}
+		})
 	}
 
 	updateStockProduct = async (dataUpdateQty) => {
